7227e64c4753b262df6e990a893d5b9e9a6d4bf0
David Valdez <me@gnuget.org>
2009-01-25 22:46:44 -0600

    Adding dynamic import for plugins in the bot

--- plugin.py
+++ plugin.py
@@ -1,6 +1,21 @@
-from plugins import math
-from plugins import flood
+import os
 
-def main(name, msg, user, advice, channel, irc):
-	math.main(msg, channel, irc)
-	flood.main(msg, channel, name, irc)
+#class  plugins:
+def main(name,msg,user,advice,channel,irc): 
+    plugins =  os.listdir("plugins")
+
+
+    #para que se incluyan solo una vez los modulos
+    included = False
+    if included == False:
+        for element in plugins:
+            if element != "__init__.py":
+                if element[len(element)-3:] == ".py":
+                    exec("from plugins import "+ element[:-3])
+    included = True 
+
+
+    for element in plugins:
+        if element != "__init__.py":
+            if element[len(element)-3:]  == ".py":
+                exec(element[:-3] + ".main(msg,channel,irc,name)")
