b77a9ba3d70ff28fab3191dd14dcd4b917c8e387
Azag <rmasad@gmail.com>
2009-02-18 13:16:08 -0300

    talk bug fix

--- plugins/talk.py
+++ plugins/talk.py
@@ -6,7 +6,7 @@ def main(data, irc):
 		pass
 	else:
 		if data["msg"] == "JOIN" and data["msg_nick"].lower() != data["bot_nick"].lower():
-			if data["msg_nick"] in data["womans"]:
+			if data["msg_nick"].lower() in data["womans"].lower():
 				IrcSend(irc, "PRIVMSG {0} :Hola señora {1}\r\n".format(data["msg_channel"], data["msg_nick"]))
 			else:
 				IrcSend(irc, "PRIVMSG {0} :Hola señor {1}\r\n".format(data["msg_channel"], data["msg_nick"]))
